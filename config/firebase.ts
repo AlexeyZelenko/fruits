32c6a17bf67344f3a31331ca1ecd196060dd61ac
Alexey Zelenko <zelenlooleksii.75@gmail.com>
2025-03-14 15:35:58 +0200

    env

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-2qvf4wgkcgpbDyaJu_iLFGkBAZ-QM1g",
-  authDomain: "fruits-test-f250a.firebaseapp.com",
-  projectId: "fruits-test-f250a",
-  storageBucket: "fruits-test-f250a.firebasestorage.app",
-  messagingSenderId: "993900687184",
-  appId: "1:993900687184:web:431a1b5ff3b5f02b21c655"
+  apiKey: process.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
